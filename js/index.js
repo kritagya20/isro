3280aff27f015437f74c75b81cafd0c0d82e85f8
Kritagya Singh Chouhan <kritagyachouhan20@gmail.com>
2022-02-12 20:39:20 +0530

    Successfully created MOON-PAGE of the website with complete responsiveness for all screen sizes

--- js/index.js
+++ js/index.js
@@ -12,3 +12,26 @@ navToggle.addEventListener("click",() =>{
         navToggle.setAttribute("aria-expanded", false);
     }
 })
+
+//extra details of the mission visibility through button
+const ExtraInfoMoon = document.querySelector(".moon-info-extra-p");
+const InfoBtn = document.querySelector(".moon-info-extra-btn");
+
+InfoBtn.addEventListener("click",() =>{
+    const visibility = ExtraInfoMoon.getAttribute("data-visible");
+    if (visibility === "false") {
+        // changes the attribute to false
+        ExtraInfoMoon.setAttribute("data-visible", true);
+        // removes no-display class display of additional information
+        ExtraInfoMoon.classList.remove("no-display");
+        // changes button text to Hide Details
+        document.getElementById("extra-btn").innerText = "Hide Details";
+    } else {
+        // changes the attribute to True
+        ExtraInfoMoon.setAttribute("data-visible", false);
+        // Adds no-display class to stop display of additional information
+        ExtraInfoMoon.classList.add("no-display");
+        // changes button text to Show Details
+        document.getElementById("extra-btn").innerText = "Show Details";
+    }
+})
